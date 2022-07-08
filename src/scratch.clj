72ed640c5155b9deca8df23b0f7f480080a7e530
FiVo <finn.volkel@gmail.com>
2022-07-08 23:20:55 +0200

    xtdb 1.21 ingest issue

--- src/scratch.clj
+++ src/scratch.clj
@@ -1,5 +1,22 @@
 (ns scratch
-  (:gen-class))
+  (:require [clojure.java.io :as io]
+            [xtdb.api :as xt]))
 
-(defn -main [& args]
-  (println "Hello from Clojure!!! " args))
+(defn start-xtdb! []
+  (letfn [(kv-store [dir]
+            {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
+                        :db-dir (io/file dir)
+                        :sync? true}})]
+    (xt/start-node
+     {:xtdb/tx-log (kv-store "data/tx-log")
+      :xtdb/document-store (kv-store "data/doc-store")
+      :xtdb/index-store (kv-store "data/index-store")})))
+
+(def xtdb-node (start-xtdb!))
+
+(def data (read-string (slurp "resources/entities.edn")))
+
+(do
+  (xt/submit-tx xtdb-node (doall (map #(vector ::xt/put %) data)))
+  ;; (xt/sync xtdb-node)
+  (.close xtdb-node))
